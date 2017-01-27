ed8e98b095390ccf09c9d2b4cf252b5692d65e36
Faydiamond <faba_1512@hotmail.com>
2017-01-27 12:05:34 -0500

    learning AngularJs

---
+++ MyTestController.js
@@ -0,0 +1,7 @@
+//Angular Controller
+var Api=angular.module("MyTest",[]);
+Api.controller("Bikes",function($scope){
+	$scope.Brand="Haro";
+	$scope.Brand1="We the people";
+	$scope.Brand2="Odissey";
+});
